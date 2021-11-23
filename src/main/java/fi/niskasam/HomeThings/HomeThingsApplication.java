b41aba2aa1cd12605ae5c287035af629b556749f
niskasam <samnisk@outlook.com>
2021-11-24 01:33:57 +0200

    Fixed some errors, 24.11.2021

--- src/main/java/fi/niskasam/HomeThings/HomeThingsApplication.java
+++ src/main/java/fi/niskasam/HomeThings/HomeThingsApplication.java
@@ -3,6 +3,7 @@ package fi.niskasam.HomeThings;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class HomeThingsApplication {
 
--- src/main/java/fi/niskasam/HomeThings/domain/CategoryRepository.java
+++ src/main/java/fi/niskasam/HomeThings/domain/CategoryRepository.java
@@ -1,9 +1,11 @@
-package fi.niskasam.HomeThings.domain;
-
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CategoryRepository extends CrudRepository<Category, Long>{
-	List<Category> findByName(String name);
-}
+//package fi.niskasam.HomeThings.domain;
+//
+//import java.util.List;
+//
+//import org.springframework.data.repository.CrudRepository;
+//
+//import groovy.lang.Category;
+//
+//public interface CategoryRepository extends CrudRepository<Category, Long>{
+//	List<Category> findByName(String name);
+//}
